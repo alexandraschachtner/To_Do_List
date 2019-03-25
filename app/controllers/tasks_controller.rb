639afb4481144647fd3bd35bcc2585c532872caa
Alexandra <alexandra.schachtner@gmail.com>
2019-03-25 13:00:58 -0500

    CRU requirements done, stopping point  5.12

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,16 +8,32 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-def new
-end
+  def new
+    @task = Task.new
+  end
 
+  def edit
+      @task = Task.find(params[:id])
+  end
 
-def create
-  @task = Task.new(task_params)
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task
+    else
+      render 'new'
+    end
+  end
 
-  @task.save
-  redirect_to @task
-end
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
+   end
+
 
 private
   def task_params
