a167fb4a779815a9b075ceeeef9a05e659d74ffb
Alexandra <alexandra.schachtner@gmail.com>
2019-03-26 19:07:38 -0500

    comments added to varuous pages and tabbing changed in task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,43 +4,47 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+
   def show
     @task = Task.find(params[:id])
   end
+
 
   def new
     @task = Task.new
   end
 
+
   def edit
       @task = Task.find(params[:id])
   end
 
+
   def create
     @task = Task.new(task_params)
-    if @task.save
-      redirect_to @task
-    else
-      render 'new'
-    end
+      if @task.save
+        redirect_to @task
+      else
+        render 'new'
+      end
   end
+
 
   def update
     @task = Task.find(params[:id])
-    if @task.update(task_params)
-      redirect_to @task
-    else
-      render 'edit'
-    end
+      if @task.update(task_params)
+        redirect_to @task
+      else
+        render 'edit'
+      end
    end
 
-
-def destroy
-  @task = Task.find(params[:id])
-  @task.destroy
-
-  redirect_to tasks_path
-end
+# the destroy method may be refered to as delete or complete in app
+ def destroy
+   @task = Task.find(params[:id])
+    @task.destroy
+     redirect_to tasks_path
+ end
 
 
 private
